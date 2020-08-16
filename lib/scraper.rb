ea7ed799ff982505ae7811498d5be30d9ec5f8cb
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-08-16 23:57:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
-
+ 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+ 
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+ 
+courses.each do |course|
+  puts course.text.strip
+end
